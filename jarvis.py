606c7e493903a228b274bd856a480f2e1665d273
akash singh <akashsingh14032000@gmail.com>
2022-03-31 19:09:56 +0530

    second commit

--- jarvis.py
+++ jarvis.py
@@ -6,7 +6,7 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 print(voices[0].id)
 
-def speak(audio):
+def speak(audio):#using audio with source
     engine.say(audio)
     engine.runAndWait()
 
